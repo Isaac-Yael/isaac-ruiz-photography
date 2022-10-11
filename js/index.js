f526af46195eb8b1dfd9185c1a59e672b10377bf
Isaac Yael <sigan1998@gmail.com>
2022-10-11 15:13:27 -0500

    Pequeño avance en JavaScript

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+const gridItem1 = document.getElementById("portfolio_grid_3")
+const gridItem2 = document.getElementById("portfolio_grid_4")
+const gridItem3 = document.getElementById("portfolio_grid_5")
+const gridItem4 = document.getElementById("portfolio_grid_6")
+const gridItem5 = document.getElementById("portfolio_grid_7")
+const changePortfolio = document.getElementById("portfolio_grid_8")
+
+const editorialPublicitaria = document.getElementById("editorialPublicitaria")
+const shootingPersonal = document.getElementById("shootingPersonal")
+const catalogo = document.getElementById("catalogo")
+const documental = document.getElementById("documental")
+const eventosSociales = document.getElementById("eventosSociales")
+
+changePortfolio.addEventListener("click", changeGrid)
+
+function changeGrid() {
+    console.log("Hola")
+}
